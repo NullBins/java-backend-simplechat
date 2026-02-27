8b2b2aff82cb4637aa1d8927b02ef0731a6c26c6
NullBins <choitsysprep@gmail.com>
2026-02-27 21:50:55 +0900

    Commit to SimpleChat Phase II

--- README.md
+++ README.md
@@ -14,5 +14,33 @@ ant run-client
 ```
 
 ## SimpleChat Phase II
+- [ **command line** ]
+```bash
+cd simplechat
+ant jar-server
+ant jar-client
+ant run-server -Darg=1234      # Argument is port, Default value: 5555
+ant run-client -Darg=bob       # Argument is username, Default value: ${arg}
+```
+- [ **server process line** ]
+>```
+>#stop    -> Stop listening for client access
+>#start   -> Start listening for client access
+>#quit    -> Leave process
+>#close   -> Server shutdown
+>#getport -> Current server access port
+>#setport -> Chage server access port
+>[just text] (ex. test)    -> SERVER MESSAGE> test
+>```
+- [ **client process line** ]
+>```
+>#login [username]    -> ID Login
+>#quit    -> Leave process
+>#logoff  -> Close client connection
+>#getport -> Client current connection port
+>#setport -> Change the connection port from client
+>#gethost -> Client current connection host
+>#sethost -> Change the connection host from client
+>```
 
 ## SimpleChat Phase III
--- src/client/ClientConsole.java
+++ src/client/ClientConsole.java
@@ -6,57 +6,52 @@ import common.*;
 
 public class ClientConsole implements ChatIF {
 	final public static int DEFAULT_PORT = 5555;
-
 	ChatClient client;
 
-	public ClientConsole(String host, int port)
-	{
-		try {
-      System.out.println("Connecting to " + host + ":" + port + "...");
-			client = new ChatClient(host, port, this);
-		} catch(IOException exception) {
-			System.out.println("Error: can't setup connection!" + " Terminating client.");
-      System.out.println("Error: " + exception.getMessage());
-      exception.printStackTrace();
-			System.exit(1);
-		}
+	public ClientConsole(String host, int port, String login) {
+		client = new ChatClient(host, port, login, this);
 	}
 
 	public void accept() {
 		try {
 			BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
-
 			String message;
-
-			while (true) {
-				message = fromConsole.readLine();
-				client.handleMessageFromClientUI(message);
-			}
+			try {
+				while (true) {
+					message = fromConsole.readLine();
+					client.handleMessageFromClientUI(message);
+				}
+			} catch (NullPointerException e) { }
 		} catch (Exception ex) {
-			System.out.println("Unexpected error while reading from console!");
+			System.out.println("Unexcepted error while reading from console!");
 		}
 	}
 
-	public void display(String message)
-	{
-		System.out.println("> " + message);
+	public void display(String message) {
+		System.out.println(message);
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		String host = "";
 		int port = 0;
-
+		String loginID = null;
 		try {
-			host = args[0];
-      if (host.equals("${arg}")) {
-        host ="127.0.0.1";
-      }
-		} catch (ArrayIndexOutOfBoundsException e) {
-			host ="127.0.0.1";
+			loginID = args[0];
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ERROR - No login ID specified. Connection aborted.");
+			System.exit(1);
 		}
-
-		ClientConsole chat = new ClientConsole(host, DEFAULT_PORT);
+		try {
+			host = args[1];
+		} catch(ArrayIndexOutOfBoundsException e) {
+			host = "localhost";
+		}
+		try {
+			port = Integer.parseInt(args[2]);
+		} catch (Throwable t) {
+			port = DEFAULT_PORT;
+		}
+		ClientConsole chat = new ClientConsole(host, port, loginID);
 		chat.accept();
 	}
 }
